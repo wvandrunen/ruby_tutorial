390f0401da1b9ea714dd07eb7e43598f046e9698
Wouter van Drunen <wvandrunen@gmail.com>
2013-11-12 10:28:58 +0100

    fixed use of Guard

--- string_calculator_kata/string_calculator.rb
+++ string_calculator_kata/string_calculator.rb
@@ -1,5 +1,3 @@
-require 'test/unit'
-
 class StringCalculator
 	def add (numbers)
 
@@ -17,31 +15,5 @@ class StringCalculator
 		end
 
 		return add
-	end
-end
-
-class TestAdd < Test::Unit::TestCase
-	def test_empty_string
-		string_cal = StringCalculator.new
-
-		assert_equal nil, string_cal.add("")
-	end
-
-	def test_add_one_and_two
-		string_cal = StringCalculator.new
-
-		assert_equal 3, string_cal.add("1,2")
-	end
-
-	def test_add_zero_and_two
-		string_cal = StringCalculator.new
-
-		assert_equal 2, string_cal.add("0,2")
-	end
-
-	def test_add_zero_and_two_and_three
-		string_cal = StringCalculator.new
-
-		assert_equal 5, string_cal.add("0,2,3")
 	end
 end
